592911e2c8fb273efd64e3a1084ec0b7d37b14e3
tomek <tomtomm77@gmail.com>
2018-05-30 11:58:52 +0200

    initial

---
+++ sorter.py
@@ -0,0 +1,18 @@
+
+
+
+
+#main loop
+while True:
+
+    # get coin - move belt until coin ni picture
+
+    # scan coin
+
+    # move to right container
+    # |0.10|0.20|0.50|1|2|5|
+
+
+    # drop coin
+
+    pass
